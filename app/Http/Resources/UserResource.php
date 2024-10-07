a077a5979853143b9092723dba304dc98c47cc36
darkpsai <darkpsai@gmail.com>
2024-10-08 00:41:14 +0800

    update user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,7 +19,6 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            // 'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
         ];
     }
 }
