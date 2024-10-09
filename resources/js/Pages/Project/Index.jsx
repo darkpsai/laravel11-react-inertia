049384bbef9591b9f645d4e5479b2b2ec60ec6ac
darkpsai <darkpsai@gmail.com>
2024-10-10 02:17:33 +0800

    update project view

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,20 +1,59 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
+import { useState } from 'react';
 
-const TABLE_LABEL_MAP = [
-  'ID',
-  'Image',
-  'Name',
-  'Status',
-  'Create Date',
-  'Due Date',
-  'Created By',
-  'Actions'
-];
+const TABLE_LABEL_MAP = {
+  'id': 'ID',
+  'image': 'Image',
+  'name': 'Name',
+  'status': 'Status',
+  'created_at': 'Create Date',
+  'due_date': 'Due Date',
+  'created_by': 'Created By',
+  'actions': 'Actions'
+};
 
-export default function Index({ projects }) {
+export default function Index({ projects, queryParams }) {
+  queryParams = queryParams || {};
+  const searchFieldChange = (name, value) => {
+    if (value) {
+      queryParams[name] = value
+    } else {
+      delete queryParams[name]
+    }
+
+    router.get(route('project.index'), queryParams);
+  }
+
+  const onKeyPress = (name, e) => {
+    if (e.key !== 'Enter') return;
+
+    searchFieldChange(name, e.target.value);
+  }
+
+  const sortChanged = (name) => {
+    if (name === queryParams.sort_field) {
+      if (queryParams.sort_direction === 'asc') {
+        queryParams.sort_direction = 'desc';
+      } else {
+        queryParams.sort_direction = 'asc';
+      }
+    } else {
+      queryParams.sort_field = name;
+      queryParams.sort_direction = 'asc';
+    }
+
+    if (name === 'image' || name === 'actions' || name === 'created_by') {
+      return;
+    }
+
+    router.get(route('project.index'), queryParams);
+  }
+
   return (
     <Authenticated
       header={
@@ -25,7 +64,7 @@ export default function Index({ projects }) {
     >
       <Head title="Projects" />
 
-      <div className="py-12">
+      <div className="py-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
             <div className="p-6 text-gray-900 dark:text-gray-100">
@@ -33,9 +72,40 @@ export default function Index({ projects }) {
               <table className='w-full text-sm text-left rtl:text-right text-gray-600 dark:text-gray-400'>
                 <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500'>
                   <tr className='text-nowrap'>
-                    {TABLE_LABEL_MAP.map((label, idx) => (
-                      <th className='px-3 py-3' key={idx}>{label}</th>
+                    {Object.entries(TABLE_LABEL_MAP).map(([key, label], idx) => (
+                      <th onClick={e => sortChanged(key)} className='px-3 py-3' key={idx}>{label}</th>
                     ))}
+                  </tr>
+                </thead>
+                <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500'>
+                  <tr className='text-nowrap'>
+                    <th className='px-3 py-3'></th>
+                    <th className='px-3 py-3'></th>
+                    <th className='px-3 py-3'>
+                      <TextInput
+                        className='w-full'
+                        placeholder='Project Name'
+                        defaultValue={queryParams.name}
+                        onBlur={e => searchFieldChange('name', e.target.value)}
+                        onKeyPress={e => onKeyPress('name', e)}
+                      />
+                    </th>
+                    <th className='px-3 py-3'>
+                      <SelectInput
+                        className='w-full'
+                        defaultValue={queryParams.status}
+                        onChange={e => searchFieldChange('status', e.target.value)}
+                      >
+                        <option value="">Select Status</option>
+                        <option value="pending">Pending</option>
+                        <option value="in_progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                      </SelectInput>
+                    </th>
+                    <th className='px-3 py-3'></th>
+                    <th className='px-3 py-3'></th>
+                    <th className='px-3 py-3'></th>
+                    <th className='px-3 py-3'></th>
                   </tr>
                 </thead>
                 <tbody>
@@ -62,7 +132,10 @@ export default function Index({ projects }) {
                   ))}
                 </tbody>
               </table>
-              <Pagination params={projects}/>
+              {status}
+              <div className='flex flex-row'>
+                <Pagination params={projects} queryParams={queryParams} />
+              </div>
             </div>
           </div>
         </div>
